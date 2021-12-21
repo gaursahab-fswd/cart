f12b90849a4fefc4d01fe5ce01a8b15cf27b9369
itprogaur <professionalcodehub@gmail.com>
2021-12-21 12:24:52 +0530

    Code modified

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,33 +1,45 @@
-import React from "react";
-class CartItem extends React.Component{
-    render(){
-        return(
-           <div className="cart-item">
-            <div className="left-item">
-                <img style={styles.Image} />
-            </div>
-            <div className="right-item">
-                {/* Action Buttons*/}
-                <div>Phone</div>
-                <div>Rs 999/-</div>
-                <div>Qty : 1</div>
-                <div className="">
+import React from 'react';
 
-                </div>
-            </div>
-           </div>
-        )
+class CartItem extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      price: 999,
+      title: 'Mobile Phone',
+      qty: 1,
+      img: ''
     }
+  }
+  render () {
+    const { price, title, qty } = this.state;
+    return (
+      <div className="cart-item">
+        <div className="left-block">
+          <img style={styles.image} />
+        </div>
+        <div className="right-block">
+          <div style={ { fontSize: 25 } }>{title}</div>
+          <div style={ { color: '#777' } }>Rs {price} </div>
+          <div style={ { color: '#777' } }>Qty: {qty} </div>
+          <div className="cart-item-actions">
+            {/* Buttons */}
+            <img alt="increase" className="action-icons" src="https://img.icons8.com/ios/50/000000/plus--v1.png" />
+            <img alt="decrease" className="action-icons" src="https://img.icons8.com/ios/50/000000/minus.png" />
+            <img alt="delete" className="action-icons" src="https://img.icons8.com/ios-glyphs/30/000000/filled-trash.png" />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const styles = {
-
-    Image:{
-
-        height:110,
-        width:110,
-        borderRadius:20
-    }
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    background: '#ccc'
+  }
 }
 
 export default CartItem;
